8b59d888007e77d8aeccb76627e8f0cbc061f006
sagorahmeed <sagorahmeed6060@gmail.com>
2021-03-02 17:22:26 +0600

    first

--- src/App.js
+++ src/App.js
@@ -1,15 +1,11 @@
-// import React, { Component } from 'react'
-// import Card from './Card';
+import React, { Component } from 'react'
+import Card from './Card';
 
-// export default class App extends Component {
+export default class App extends Component {
 
-//    render() {
-//       let items = [];
-//         for(let x = 0; x<Data.length; x++){
-//             items.push(<Card {Data[x].heading} {Data[x].title}/>)
-//         }
-//       return (
-//         {items}
-//       )
-//    }
-// }
+   render() {
+      return (
+        <Card/>
+      )
+   }
+}
